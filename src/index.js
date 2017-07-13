056e7913f9a49c46910effcbcc66b640ae0f2978
sjones6 <jones.spencera@gmail.com>
2017-07-12 23:32:41 -0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,59 @@
+const {Flint, Adapter} = require('./gun-flint');
+const MongoClient = require('mongodb');
+
+Flint.register(new Adapter({
+    getTimeout: null,
+    putTimeout: null,
+    ready: false,
+    initialized: false,
+    get: function(key, done) {
+        if (this.initialized) {
+            let get = this.get.bind(this, key, done);
+            if (!this.ready) {
+                this.getTimeout = setTimeout(get, 500);
+            } else {
+                this.db.findOne({key}, (err, result) => {
+                    if (err) {
+                        done(this.errors.internal)
+                    } else if (!result) {
+                        done(this.errors.lost);
+                    } else {
+                        done(null, result);
+                    }
+                });
+            }
+        }
+    },
+    put: function(key, val, done) {
+        if (this.initialized) {
+            let put = this.put.bind(this, key, val, done);
+            if (!this.ready) {
+                this.putTimeout = setTimeout(put, 500);
+            } else {
+                this.db.insertOne({key, val: JSON.stringify(val)}, done);
+            }
+        }
+    },
+    opt: function(context, opt, done) {
+        let {mongo} = opt;
+        if (mongo) {
+            this.initialized = true;
+            let database = mongo.database || 'gun';
+            let port = mongo.port || '27017';
+            let host = mongo.host || 'localhost';
+            let query = mongo.query ? '?' + mongo.query : '';
+            this.collection = mongo.collection || 'gun-mongo';
+            
+            let _self = this;
+            MongoClient.connect(`mongodb://${host}:${port}/${database}${query}`, (err, db) => {
+                if (err) {
+                    throw err;
+                }
+                _self.ready = true;
+                _self.db = db.collection(_self.collection);
+            });
+        } else {
+            this.initialized = false
+        }
+    }
+}));
